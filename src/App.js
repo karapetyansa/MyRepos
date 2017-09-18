5eab8b98b5643db5c4cad162db2aa09a60ca7149
User <sashamangir@mail.ru>
2017-09-18 14:34:28 +0300

    создали папку для работы с запросом

--- src/App.js
+++ src/App.js
@@ -1,43 +1,44 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react'
+import './App.css'
 
-import "bootswatch/paper/bootstrap.css";
+import 'bootswatch/paper/bootstrap.css'
 
-import { Navbar, NavItem, Nav, Grid, Row, Col } from "react-bootstrap";
+import { Navbar, NavItem, Nav, Grid, Row, Col } from 'react-bootstrap'
 
 import Img from 'react-image'
-import photo from './photo.png'
-console.log(photo)
 
 const PLACES = [
-  { name: "Bender"},
-  { name: "Ribnita"},
-  { name: "Dubasari"},
-  { name: "Chisinau"},
-  {name: "Tiraspol"}
-];
+  { name: 'Bender' },
+  { name: 'Ribnita' },
+  { name: 'Dubasari' },
+  { name: 'Chisinau' },
+  { name: 'Tiraspol' }
+]
 
 class WeatherDisplay extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       weatherData: null
-    };
+    }
   }
   componentDidMount() {
-    const name = this.props.name;
-    const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    const name = this.props.name
+    const URL =
+      'http://api.openweathermap.org/data/2.5/weather?q=' +
       name +
-      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
-    fetch(URL).then(res => res.json()).then(json => {
-      this.setState({ weatherData: json });
-    });
+      '&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric'
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({ weatherData: json })
+      })
   }
   render() {
-    const weatherData = this.state.weatherData;
-    if (!weatherData) return <div>Loading</div>;
-    const weather = weatherData.weather[0];
-    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    const weatherData = this.state.weatherData
+    if (!weatherData) return <div>Loading</div>
+    const weather = weatherData.weather[0]
+    const iconUrl = 'http://openweathermap.org/img/w/' + weather.icon + '.png'
     return (
       <div>
         <h1>
@@ -49,31 +50,32 @@ class WeatherDisplay extends Component {
         <p>Low: {weatherData.main.temp_min}°</p>
         <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
       </div>
-    );
+    )
   }
 }
 
 class App extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       activePlace: 0
-    };
+    }
   }
   render() {
-    const activePlace = this.state.activePlace;
+    const activePlace = this.state.activePlace
     return (
       <div>
         <Navbar>
           <Navbar.Header>
-            <Img src = {photo} height="45" width="45" circle />
             <Nav pullRight>
-              <NavItem eventKey={1} href="#">Sign IN</NavItem>
-              <NavItem eventKey={2} href="#">Sign UP</NavItem>
-              </Nav>
-            <Navbar.Brand>
-              Weather by Sasha for Karapetian S.A.
-            </Navbar.Brand>
+              <NavItem eventKey={1} href="#">
+                Sign IN
+              </NavItem>
+              <NavItem eventKey={2} href="#">
+                Sign UP
+              </NavItem>
+            </Nav>
+            <Navbar.Brand>Weather Sasha</Navbar.Brand>
           </Navbar.Header>
         </Navbar>
         <Grid>
@@ -85,22 +87,27 @@ class App extends Component {
                 stacked
                 activeKey={activePlace}
                 onSelect={index => {
-                  this.setState({ activePlace: index });
+                  this.setState({ activePlace: index })
                 }}
               >
                 {PLACES.map((place, index) => (
-                  <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                  <NavItem key={index} eventKey={index}>
+                    {place.name}
+                  </NavItem>
                 ))}
               </Nav>
             </Col>
             <Col md={8} sm={8}>
-              <WeatherDisplay key={activePlace} name={PLACES[activePlace].name} />
+              <WeatherDisplay
+                key={activePlace}
+                name={PLACES[activePlace].name}
+              />
             </Col>
           </Row>
         </Grid>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
