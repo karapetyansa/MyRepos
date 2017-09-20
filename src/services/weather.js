8780c686dee3324c8771949c8e27791f5844a26c
karapetyansa <karapetyan.sa@gmail.com>
2017-09-20 16:18:50 +0300

    5

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,5 +1,5 @@
 export const getWeatherFromNet = async cityName => {
-  const URL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=02209aa59ed5efd0d2976605f455a257&units=metric`
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=02209aa59ed5efd0d2976605f455a257&units=metric`
   const res = await fetch(URL)
   return res.json()
 }
